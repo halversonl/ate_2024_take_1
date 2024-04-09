6ea3734f9185d82e07a38ec909f81319ee8d4cd0
leohalverson <halversonl@educ.college-montreal.qc.ca>
2024-04-09 14:49:54 -0400

    started work on forms and file uploading.

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .forms import UploadFileForm
 
 # Create your views here.
 def homepage_view(request, *args, **kwargs):
-    #return HttpResponse("<h1>i hate programming so god damn much</h1>")
+    # return HttpResponse("<h1>i hate programming so god damn much</h1>")
     return render(request, "homepage.html")
+
 
 def about_view(*args, **kwargs):
     return HttpResponse("<h1>this project is about making me as miserable as humanly fucking possible</h1>")
+
+def upload_file(request):
+    pass
