de43850df2d0fae9cfc10b22bbf7b69d3f4ea45a
leohalverson <halversonl@educ.college-montreal.qc.ca>
2024-05-06 10:44:48 -0400

    finished activity model. now time to figure out a way to process the activities in the database

--- activity_input/models.py
+++ activity_input/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Activity(models.Model):
-    title = models.TextField()
-    date = models.TextField()
+    title = models.CharField(max_length=120)
+    date = models.DateTimeField(null=True, blank=True)
     description = models.TextField()
     file = models.FileField()
